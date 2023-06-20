f8c032fbc2d0a664ad9c0f4160091e34c4d5f448
Nick Fox-Gieg <nick@fox-gieg.com>
2023-06-20 12:19:29 -0400

    shell

--- setup.bat
+++ setup.bat
@@ -7,7 +7,7 @@ git submodule foreach git checkout master
 git submodule foreach git reset --hard
 git submodule foreach git pull origin master
 
-set BASE_URL="https://fox-gieg.com/patches/github/n1ckfg/latkUnity_ViveSR/Assets"
+set BASE_URL="https://fox-gieg.com/patches/github/n1ckfg/latkUnity_ViveSR/Assets/ViveSR"
 cd %~dp0
 
 powershell -Command "Invoke-WebRequest %BASE_URL%/Plugins.zip -OutFile Plugins.zip"
--- setup.command
+++ setup.command
@@ -17,7 +17,7 @@ git submodule foreach git checkout master
 git submodule foreach git reset --hard
 git submodule foreach git pull origin master
 
-BASE_URL="https://fox-gieg.com/patches/github/n1ckfg/latkUnity_ViveSR/Assets"
+BASE_URL="https://fox-gieg.com/patches/github/n1ckfg/latkUnity_ViveSR/Assets/ViveSR"
 cd Assets
 wget "$BASE_URL/Plugins.zip"
 unzip Plugins.zip
